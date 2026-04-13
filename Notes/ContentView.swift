e7b30863b595ba1e989a81576554d500c9de397d
lopezcodeus <lopez.code.us@gmail.com>
2026-04-13 19:43:43 -0400

    Ellipse

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Rectangle()
-            .frame(width: 200, height: 200)
-            .foregroundColor(.yellow)
-            .cornerRadius(8)
-            .opacity(0.5)
+            Ellipse()
+            .stroke(lineWidth: 5, )
+            .foregroundStyle(.red)
+            .frame(width: 200, height: 400)
+            .rotationEffect(.degrees(30))
+            
     }
 }
 
