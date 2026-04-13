c883f46fda6e5c6555c82ac4b8ca4054f10fa7c7
admin <admin@Mac.lan>
2026-04-13 18:15:29 -0400

    initial commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by admin on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
