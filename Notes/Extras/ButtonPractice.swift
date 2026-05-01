5ddcd82129bc626dfef1dede5839c115ef82dcad
lopezcodeus <lopez.code.us@gmail.com>
2026-05-01 16:01:17 -0400

    Custom Button

--- Notes/Extras/ButtonPractice.swift
+++ Notes/Extras/ButtonPractice.swift
@@ -12,10 +12,20 @@ struct ButtonPractice: View {
     @State var counter: Int = 0
     
     var body: some View {
-        Button("Aumentar") {
-            self.counter += 1
-            print("El boton esta funcionando: \(self.counter)")
-        }
+        VStack {
+            Text("Contador: \(self.counter)")
+            Button {
+                self.counter += 1
+            } label: {
+                Text("Aumentar")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+        }.padding(30)
     }
 }
 
