3e95ae3fc7fe647b317b7aecd8ade92980b76115
lopezcodeus <lopez.code.us@gmail.com>
2026-05-01 15:39:13 -0400

    Add Button

---
+++ Notes/Extras/ButtonPractice.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonPractice.swift
+//  Notes
+//
+//  Created by admin on 5/1/26.
+//
+
+import SwiftUI
+
+struct ButtonPractice: View {
+    var body: some View {
+        Button("Press me") {
+            print("El boton esta funcionando")
+        }
+    }
+}
+
+#Preview {
+    ButtonPractice()
+}
